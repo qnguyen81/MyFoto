0496b135afa94c5b4d4ecc7b6ecb253844938a17
qnguyen81 <hoangquan2561999hl@gmail.com>
2019-03-13 14:20:42 -0500

    all forms created

--- Login.php
+++ Login.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Create Account</title>
+    <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
@@ -18,48 +18,46 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
 </head>
 <body>
-    <div class="container">
+
+      <div class="container">
         <div class="row">
 			<div class="col-md-5 mx-auto">
 			<div id="first">
 				<div class="myform form ">
 					 <div class="logo mb-3">
 						 <div class="col-md-12 text-center">
-							<h1>Create Account</h1>
+							<h1>Login</h1>
 						 </div>
 					</div>
                    <form action="" method="post" name="login">
-                   <div class="form-group">
-                              <label for="exampleInputEmail1">Account</label>
-                              <input type="account" name="account" id="account"  class="form-control" aria-describedby="emailHelp" placeholder="Enter Account">
-                           </div>
-                           <div class="form-group">
-                              <label for="exampleInputEmail1">Password</label>
-                              <input type="password" name="password" id="password"  class="form-control" aria-describedby="emailHelp" placeholder="Enter Password">
-                           </div>
-                           <div class="form-group">
-                              <label for="exampleInputEmail1">Confirm Password</label>
-                              <input type="con-password" name="con-password" id="con-password"  class="form-control" aria-describedby="emailHelp" placeholder="Enter Password Again">
-                           </div>
                            <div class="form-group">
                               <label for="exampleInputEmail1">Email address</label>
                               <input type="email" name="email"  class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
                            </div>
                            <div class="form-group">
+                           <a class="float-right" href="#">Forgot?</a>
+                              <label for="exampleInputEmail1">Password</label>
+                              <input type="password" name="password" id="password"  class="form-control" aria-describedby="emailHelp" placeholder="Enter Password">
+                           </div>
+                           <div class="form-group">
                               <!-- <p class="text-center">By signing up you accept our <a href="#">Terms Of Use</a></p> -->
                            </div>
                            <div class="col-md-12 text-center ">
-                              <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm">Create Account</button>
+                              <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm">Login</button>
                            </div>
                            <div class="col-md-12 ">
                               <div class="login-or">
                                  <hr class="hr-or">
                               </div>
                            </div>
+                           <div class="col-md-12 mb-3">
+                           </div>
+                           <div class="form-group">
+                              <p class="text-center">Don't have account? <a href="newAccount.php" id="signup">Sign up here</a></p>
+                           </div>
                         </form>
                 
 				</div>
 			</div> 
-    
 </body>
 </html>
